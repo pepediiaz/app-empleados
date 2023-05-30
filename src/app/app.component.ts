ac46528dc8ae8110d595fd160798a1f5d03fd605
Agustin Diez <agustindxm@gmail.com>
2023-05-30 19:29:11 -0300

    output en el componente hijo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,35 +4,35 @@ import { Empleado } from './empleado.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  titulo= "Lista de Empleados";
-  
-  empleados:Empleado[]=[
+  titulo = 'Lista de Empleados';
 
-  new Empleado("Juan","Díaz","Presidente",7500),
-  new Empleado("Pepe","Díaz","Directora",5500),
-  new Empleado("Sara","Martín","Encargada",3500),
-  new Empleado("Laura","Jimenez","Vicepresidente",2500),
-
+  empleados: Empleado[] = [
+    new Empleado('Juan', 'Díaz', 'Presidente', 7500),
+    new Empleado('Pepe', 'Díaz', 'Directora', 5500),
+    new Empleado('Sara', 'Martín', 'Encargada', 3500),
+    new Empleado('Laura', 'Jimenez', 'Vicepresidente', 2500),
   ];
 
-  agregarEmpleado(){
-
-    let miEmpleado=new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
+  agregarEmpleado() {
+    let miEmpleado = new Empleado(
+      this.cuadroNombre,
+      this.cuadroApellido,
+      this.cuadroCargo,
+      this.cuadroSalario
+    );
     this.empleados.push(miEmpleado);
-
   }
 
-
-
   borrarEmpleado(): void {
-    const empleadoExistente = this.empleados.find(empleado =>
-      empleado.nombre === this.cuadroNombre &&
-      empleado.apellido === this.cuadroApellido &&
-      empleado.cargo === this.cuadroCargo &&
-      empleado.salario === this.cuadroSalario
+    const empleadoExistente = this.empleados.find(
+      (empleado) =>
+        empleado.nombre === this.cuadroNombre &&
+        empleado.apellido === this.cuadroApellido &&
+        empleado.cargo === this.cuadroCargo &&
+        empleado.salario === this.cuadroSalario
     );
 
     if (empleadoExistente) {
@@ -40,18 +40,19 @@ export class AppComponent {
       this.empleados.splice(index, 1);
     }
   }
+
   eliminarEmpleado(index: number): void {
     if (index >= 0 && index < this.empleados.length) {
       this.empleados.splice(index, 1);
     }
   }
 
+  onEmpleadoBorrado(empleadoId: number): void {
+    this.empleados.splice(empleadoId, 1);
+  }
 
-
-
-  cuadroNombre:string="";
-  cuadroApellido:string="";
-  cuadroCargo:string="";
-  cuadroSalario:number=0;
+  cuadroNombre: string = '';
+  cuadroApellido: string = '';
+  cuadroCargo: string = '';
+  cuadroSalario: number = 0;
 }
-
--- src/app/empleado-hijo-c/empleado-hijo-c.component.ts
+++ src/app/empleado-hijo-c/empleado-hijo-c.component.ts
@@ -1,27 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Empleado } from '../empleado.model';
 
 @Component({
   selector: 'app-empleado-hijo-c',
   templateUrl: './empleado-hijo-c.component.html',
-  styleUrls: ['./empleado-hijo-c.component.css']
+  styleUrls: ['./empleado-hijo-c.component.css'],
 })
-export class EmpleadoHijoCComponent implements OnInit{
-
-  @Input() empleadoDeLista:Empleado;
-
-  @Input() indice:number;
-
+export class EmpleadoHijoCComponent implements OnInit {
+  @Input() empleadoDeLista: Empleado;
+  @Input() indice: number;
+  @Output() empleadoBorrado: EventEmitter<number> = new EventEmitter<number>();
 
   eliminarEmpleado(index: number): void {
-    if (index >= 0 && index < this.empleadoDeLista.length) {
-      this.empleadoDeLista.splice(index, 1);
-    }
+    this.empleadoBorrado.emit(this.indice);
   }
 
   constructor() {}
 
   ngOnInit(): void {}
-
-
 }
